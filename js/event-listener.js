d7e7ab860f9b281e6a302e92020034755a4d2088
verdantpath <m_dittmann@yahoo.com>
2017-11-01 14:52:04 -0700

    add js

---
+++ js/event-listener.js
@@ -0,0 +1,35 @@
+function checkUsername() {
+  // GET FEEDBACK ELEMENT
+  var elMsg = document.getElementById('feedback');
+  // IF USERNAME IS TOO SHORT
+  if (this.value.length < 5) {
+    // SET MESSAGE
+    elMsg.textContent = "Username must be 5 characters or more";
+    // IF USERNAME IS NOT TOO SHORT
+  } else {
+    // CLEAR THE MESSAGE
+    elMsg.textContent = "";
+  }
+}
+
+/*
+
+Does this affect the ratio in github????
+Does this affect the ratio in github????
+Does this affect the ratio in github????
+Does this affect the ratio in github????
+Does this affect the ratio in github????
+Does this affect the ratio in github????
+
+*/
+
+
+
+
+
+
+
+// GET THE INPUT VALUE FROM THE USERNAME FIELD
+var elUsername = document.getElementById('username');
+// WHEN THE FIELD LOSES FOCUS, CALL checkUsername()
+elUsername.addEventListener('blur', checkUsername);
